b63c861c426248a7978f0ac1274d44bbc4bb809f
sj-jason-liu <sj.jason.liu@gmail.com>
2021-07-18 23:48:05 +0800

    Created wall jumping function & pushable box function with pressure pad.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,9 @@ public class Player : MonoBehaviour
 {
     private CharacterController _controller;
 
+    private Vector3 _movement, _velocity;
+    private Vector3 _wallSurfaceNormal;
+
     [SerializeField]
     private float _moveSpeed = 5f;
     [SerializeField]
@@ -14,12 +17,15 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _jumpHeight = 15f;
     private float _yVelocity;
+    [SerializeField]
+    private float _pushPower = 3f;
 
     //variable for player coins
     private int _coin = 0;
     private int _lives = 3;
 
     private bool _hasDoubleJump;
+    private bool _canWallJump;
 
     void Start()
     {
@@ -31,11 +37,14 @@ public class Player : MonoBehaviour
     void Update()
     {
         float horizontalMove = Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(horizontalMove, 0, 0);
-        Vector3 velocity = movement * _moveSpeed;
+        
         if (_controller.isGrounded)
         {
+            _movement = new Vector3(horizontalMove, 0, 0);
+            _velocity = _movement * _moveSpeed;
+
             _hasDoubleJump = false;
+            _canWallJump = false;
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 _yVelocity = _jumpHeight;
@@ -43,10 +52,18 @@ public class Player : MonoBehaviour
         }
         else
         {
-            if (!_hasDoubleJump && Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                _yVelocity += _jumpHeight;
-                _hasDoubleJump = true;
+                if(!_hasDoubleJump && !_canWallJump)
+                {
+                    _yVelocity += _jumpHeight;
+                    _hasDoubleJump = true;
+                }
+                else if(_canWallJump)
+                {
+                    _yVelocity = _jumpHeight;
+                    _velocity = _wallSurfaceNormal * _moveSpeed;
+                }
             }
             else
             {
@@ -54,8 +71,27 @@ public class Player : MonoBehaviour
             }
         }
 
-        velocity.y = _yVelocity;
-        _controller.Move(velocity * Time.deltaTime);
+        _velocity.y = _yVelocity;
+        _controller.Move(_velocity * Time.deltaTime);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.transform.tag == "MovingBox")
+        {
+            Rigidbody rb = hit.collider.attachedRigidbody;
+            if(rb != null)
+            {
+                Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, 0);
+                rb.velocity = _pushPower * pushDir;
+            }
+        }
+        
+        if (_controller.isGrounded == false && hit.transform.tag == "Wall")
+        {
+            _wallSurfaceNormal = hit.normal;
+            _canWallJump = true;
+        }
     }
 
     public void CoinUpdate()
---
+++ Assets/Scripts/PressurePad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePad : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "MovingBox")
+        {
+            if(Vector3.Distance(transform.position, other.transform.position) < 0.1f)
+            {
+                Rigidbody rb = other.GetComponent<Rigidbody>();
+                if(rb != null)
+                {
+                    rb.isKinematic = true;
+                }
+                MeshRenderer meshRenderer = gameObject.GetComponentInChildren<MeshRenderer>();
+                if(meshRenderer != null)
+                {
+                    meshRenderer.material.color = Color.blue;
+                }
+            }
+        }
+    }
+}
